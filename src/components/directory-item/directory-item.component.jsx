3cb84ec52333e9269e281620803cebe3c83cf461
Jayant Kodam <38130397+Jayant22@users.noreply.github.com>
2023-01-22 19:12:26 +0530

    Final Fixes and added the Redirect for Netlify

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,17 @@
 import './directory-item.styles.scss';
+import { useNavigate } from 'react-router-dom';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
   return (
-    <div key={category.id} className='directory-item-container'>
+    <div
+      key={category.id}
+      className='directory-item-container'
+      onClick={onNavigateHandler}
+    >
       <div
         className='background-image'
         style={{
